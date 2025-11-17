9e42c1b7cd14b2bb5e732c79dde54c3163b5caef
kenan0817 <kananbuttaev001@gmail.com>
2025-11-17 17:11:35 +0400

    blog_list.html dinamiklesdirildi.'Add article' sehifede gorunur

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Article
 # Create your views here.
 def blog_list(request):
-    return render(request,'blog/blog_list.html')
+    
+    articles=Article.objects.filter(is_published=True).order_by('-created_at')
+    
+    context = {
+        "articles":articles
+    }
+    return render(request,'blog/blog_list.html',context)
